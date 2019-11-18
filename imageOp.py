659f9fd5b1de0f73cfd14fa4cb7ec21b0364f34e
abhinav2188 <39943422+abhinavchmod777@users.noreply.github.com>
2019-11-18 23:05:43 +0530

    negative image

--- imageOp.py
+++ imageOp.py
@@ -9,13 +9,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+#reading image in grayscale
 img = cv2.imread('img1.jpg',0)
 
 #to autosize the image window
 cv2.namedWindow('image',cv2.WINDOW_NORMAL)
 
 cv2.imshow('image',img)
+
+#to wait for a key press to exit the image window
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#to write the grayscale image into new file
 cv2.imwrite('imgGray.jpg',img)
+
+pixel = img[100,100]
+
+img_negative = cv2.bitwise_not(img)
+
+
+
+cv2.imshow('image',img_negative)
+
+#to wait for a key press to exit the image window
+cv2.waitKey(0)
+cv2.destroyAllWindows()
